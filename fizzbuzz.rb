225029478ae4b59ccabbe9a2489ae7ebe855fe8a
Mario <mabejara@gmail.com>
2020-02-05 02:51:18 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+fizz_3 = 3
+fizz_5 = 5
+Fizz_15 = 15
+
+def fizzbuzz(val)
+  if val % 3 == 0 && val % 5 == 0
+    "FizzBuzz"
+#  elsif val % 3 || 0 && val % 5 || 0
+#    nil
+  elsif val % 5 == 0
+    "Buzz"
+  elsif val % 3 == 0
+    "Fizz"
+  else val = nil
+    nil
+  end
+end
