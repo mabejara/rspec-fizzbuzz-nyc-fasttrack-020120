ae1a8089042d9916b1e384853e6df1a2b05cb6b4
Mario <mabejara@gmail.com>
2020-02-05 01:34:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+fizz_3 = 3
+def fizzbuzz(fizz_3)
+  if fizz_3/3
+    puts "Fizz"
+  end
+end
+fizzbuzz(fizz_3)
