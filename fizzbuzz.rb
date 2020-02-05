d95ddc5101fdfe1808ff9f372d7da8b5c31bca8b
Mario <mabejara@gmail.com>
2020-02-05 01:42:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 fizz_3 = 3
-def fizzbuzz(fizz_3)
-  if fizz_3/3
+def fizzbuzz(the_number)
+  if "the the_number is divisble by 3" && "the the_number is divisible by 5"
     puts "Fizz"
   end
 end
